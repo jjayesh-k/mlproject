980c14abb51a4e002683070dbe401f718c35a71e
jjayesh-k <jayeshkandar7@gmail.com>
2025-06-30 18:45:25 +0530

    EDA and Model training

--- src/exception.py
+++ src/exception.py
@@ -1,3 +1,4 @@
+# from src.logger import logging
 import logging
 import sys
 def error_message_detail(error,error_detail:sys):
@@ -15,10 +16,10 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
                 
-# if __name__ == "__main__":
+if __name__ == "__main__":
 
-#     try:
-#         a=1/0
-#     except Exception as e:
-#         logging.info("Div by 0")
-#         raise CustomException(e, sys)
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("Div by 0")
+        raise CustomException(e, sys)

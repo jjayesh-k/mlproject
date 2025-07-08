9a89675ce6c2a2cf72ce6fb8bc240e2e743d1905
jjayesh-k <jayeshkandar7@gmail.com>
2025-07-08 22:28:48 +0530

    application

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import dill
 import numpy as np
 import pandas as pd
 import pickle
@@ -48,5 +49,13 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
 
         return report
 
+    except Exception as e:
+        raise CustomException(e, sys)
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
+        
     except Exception as e:
         raise CustomException(e, sys)
